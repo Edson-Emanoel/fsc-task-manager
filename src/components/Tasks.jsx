48156240ce3543d26a1533286b3415ea63a21bef
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-11-30 17:07:08 -0300

    feat: a função de remoção de tarefas está funcionando

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -61,6 +61,14 @@ const Tasks = () => {
   }
 
   const handleTaskDeleteClick = async (taskId) => {
+    // Chama a API para deletar a tarefa
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+    if (!response.ok) {
+      return toast.error("Erro ao deletar a tarefa. Por favor, tente novamente")
+    }
+    // Atualiza o state
     const newTask = tasks.filter((task) => task.id !== taskId)
     setTasks(newTask)
     toast.success("Tarefa deletada com sucesso!")
